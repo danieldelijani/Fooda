42d1c95b7c0ed50dd30896ef571eea59990e8eb2
Daniel Delijani <danieldelijani@gmail.com>
2022-02-22 16:26:28 -0500

    added buttons:

--- FoodaApp/App.js
+++ FoodaApp/App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+// import { Button } from 'react-native-web';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Welcome to Fooda</Text>
+      <Button title="Google Maps API"/>
+      <Button title="Shopping List"/>
       <StatusBar style="auto" />
     </View>
   );
