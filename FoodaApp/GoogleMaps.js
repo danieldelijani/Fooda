1f45c4a08bb44dfa221f61bef70a20f0947054d8
Daniel Delijani <danieldelijani@gmail.com>
2022-02-27 18:39:35 -0500

    map is working

--- FoodaApp/GoogleMaps.js
+++ FoodaApp/GoogleMaps.js
@@ -1,15 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Dimensions } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
 import GroceryList from './GroceryList.js';
+import MapView from 'react-native-maps';
 
 const GoogleMaps = ({ navigation, route }) => {
     let name = route.params.name;
     return (
-      <Text>This is the Demo for Google Maps</Text>
+      <View style={styles.container}>
+        <MapView style={styles.map} />
+      </View>
     );
   };
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    map: {
+      width: Dimensions.get('window').width,
+      height: Dimensions.get('window').height,
+    },
+  });
 
   export default GoogleMaps;
