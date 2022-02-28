e27d624170b3af5894c57894f54a94ee3a32593f
Daniel Delijani <danieldelijani@gmail.com>
2022-02-28 15:01:02 -0500

    maps working with markers

--- FoodaApp/GoogleMaps.js
+++ FoodaApp/GoogleMaps.js
@@ -2,18 +2,61 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, Dimensions } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import React from 'react';
+import React, {Component} from 'react';
 import GroceryList from './GroceryList.js';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 
 const GoogleMaps = ({ navigation, route }) => {
-    let name = route.params.name;
+    // let name = route.params.name;
+
     return (
       <View style={styles.container}>
-        <MapView style={styles.map} />
+        <Map />
       </View>
     );
   };
+
+class Map extends Component {
+  constructor() {
+    super();
+    
+    this.state = {
+      region: {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421
+      },
+      markers: [
+        {
+          latlng: {latitude: 37.78825, longitude: -122.4324},
+          title: 'Foo Place',
+          description: 'Da crib'
+        }
+      ]
+    };
+    this.onRegionChange = this.onRegionChange.bind(this);
+  }
+
+  onRegionChange(region) {
+    this.setState({ region });
+  }
+
+  render() {
+    return (
+      <MapView region={this.state.region} onRegionChange={this.onRegionChange} style={styles.map}>
+        {this.state.markers.map((marker, index) => (
+          <Marker
+            key={index}
+            coordinate={marker.latlng}
+            title={marker.title}
+            description={marker.description}
+          />
+        ))}
+      </MapView>
+    );
+  };
+};
 
   const styles = StyleSheet.create({
     container: {
