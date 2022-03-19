d47716e5c7ec554328fc7eab3ffa6ed72fc01539
Daniel Delijani <danieldelijani@gmail.com>
2022-03-19 17:15:11 -0400

    added some basic styling to navigation bar

--- FoodaApp/App.js
+++ FoodaApp/App.js
@@ -11,64 +11,6 @@ export default function App() {
   )
 }
 
-// const Stack = createNativeStackNavigator();
-
-// const MyStack = () => {
-//   return (
-//     <NavigationContainer>
-//       <Stack.Navigator>
-//         <Stack.Screen
-//           name="Home"
-//           component={HomeScreen}
-//           options={{ title: 'Welcome' }}
-//         />
-//         <Stack.Screen name="Profile" component={ProfileScreen} />
-//       </Stack.Navigator>
-//     </NavigationContainer>
-//   );
-// };
-
-// export default function App() {
-//   return (
-//     <NavigationContainer>
-//       <Stack.Navigator initialRouteName="Home">
-//         <Stack.Screen name="Home" component={HomeScreen} />
-//         <Stack.Screen name="GoogleMaps" component={GoogleMaps} />
-//         <Stack.Screen name="GroceryList" component={GroceryList} />
-//       </Stack.Navigator>
-//     </NavigationContainer>
-//   );
-// }
-
-//       // {/* <View style={styles.container}>
-//       //   <Text>Welcome to Fooda</Text>
-//       //   <Button title="Google Maps API"/>
-//       //   <Button title="Shopping List"/>
-//       //   <StatusBar style="auto" />
-//       // </View> */}
-
-// const HomeScreen = ({ navigation }) => {
-//   return (
-//     <View>
-//       <Text>
-//         This is the HomeScreen
-//       </Text>
-//       <Button
-//         title="Go to Google Maps Demo"
-//         onPress={() =>
-//           navigation.navigate('GoogleMaps', { name: 'Jane' })
-//         }
-//       />
-//       <Button
-//         title="Go to Grocery List Demo"
-//         onPress={() =>
-//           navigation.navigate('GroceryList', { name: 'Jane' })
-//         }
-//       />
-//     </View>
-//   );
-// };
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
--- FoodaApp/navigation/MainContainer.js
+++ FoodaApp/navigation/MainContainer.js
@@ -33,7 +33,15 @@ export default function MainContainer() {
 
                     return <Ionicons name = {iconName} size = {size} color = {color}></Ionicons>
                 }
-            })}>
+            })}
+            
+            tabBarOptions = {{
+                activeTintColor: 'tomato',
+                inactiveTintColor: 'grey',
+                labelStyle: {paddingBottom: 10, fontSize: 10},
+                style: {padding:10, height:70}
+            }}
+            >
 
                 <Tab.Screen name = {homeName} component = {GoogleMaps}/>
                 <Tab.Screen name = {groceryListName} component = {GroceryList}/>
