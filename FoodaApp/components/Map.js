9fd0ba69de0cac030bf61cb02ef3f13973a32baf
Daniel Delijani <danieldelijani@gmail.com>
2022-04-06 20:47:04 -0400

    added the modal

--- FoodaApp/components/Map.js
+++ FoodaApp/components/Map.js
@@ -1,6 +1,6 @@
 import React, {Component, useState, useEffect} from 'react';
 import MapView, { Marker } from 'react-native-maps';
-import {StyleSheet, Dimensions} from 'react-native';
+import {StyleSheet, Dimensions, View, Modal, Text} from 'react-native';
 import * as Location from 'expo-location';
 
 
@@ -14,9 +14,14 @@ class Map extends Component {
           longitude: this.props.location["longitude"],
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421
-        }
+        },
+        modalVisible: false
       };
       this.onRegionChange = this.onRegionChange.bind(this);
+    }
+
+    setModalVisible = (visible) => {
+      this.setState({ modalVisible: visible });
     }
 
     componentDidUpdate(prevProps) {
@@ -37,17 +42,36 @@ class Map extends Component {
     }
   
     render() {
+      const modalVisible = this.state.modalVisible;
       return (
-        <MapView region={this.state.region} onRegionChange={this.onRegionChange} style={styles.map}>
-          {this.props.groceryStores.map((marker, index) => (
-            <Marker
-              key={index}
-              coordinate={marker.latlng}
-              title={marker.title}
-              description={marker.description}
-            />
-          ))}
-        </MapView>
+        <View>
+            
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+              Alert.alert("Modal has been closed.");
+              this.setModalVisible(!modalVisible);
+            }}
+            >
+              <View style={styles.modalView}>
+              <Text>Hello World</Text>
+              </View>
+          </Modal>
+            
+          <MapView region={this.state.region} onRegionChange={this.onRegionChange} style={styles.map}>
+            {this.props.groceryStores.map((marker, index) => (
+              <Marker
+                key={index}
+                coordinate={marker.latlng}
+                title={marker.title}
+                description={marker.description}
+                onPress={e => this.setModalVisible(true)}
+              />
+            ))}
+          </MapView>
+        </View>
       );
     };
   };
@@ -58,6 +82,23 @@ container: {
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+},
+modalView: {
+  margin: 20,
+  backgroundColor: "white",
+  borderRadius: 20,
+  padding: 35,
+  alignItems: "center",
+  shadowColor: "#000",
+  shadowOffset: {
+    width: 0,
+    height: 2
+  },
+  shadowOpacity: 0.25,
+  shadowRadius: 4,
+  elevation: 5,
+  height: 700,
+  width: 700
 },
 map: {
     width: Dimensions.get('window').width,
