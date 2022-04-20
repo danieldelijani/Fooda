38219c35eb06111aee675e275c8332561f10e1d1
samzorps <samzorpette@gmail.com>
2022-04-20 14:16:19 -0400

    added styling for modal

--- FoodaApp/components/StoreView.js
+++ FoodaApp/components/StoreView.js
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
-import {View, Text, StyleSheet, Button, Modal, Dimensions} from 'react-native';
+import {View, StyleSheet, Button, Modal, Dimensions} from 'react-native';
+import { Avatar, Card, IconButton, Divider, Text } from 'react-native-paper';
+
 
 const StoreView = (props) => {
     console.log(props.storeInfo);
@@ -8,6 +10,13 @@ const StoreView = (props) => {
     items_available = Math.floor(Math.random() * 18);
     items_total = items_available + 1;
     total_price = Math.round(Math.random() * 10000) / 100;
+    const store_name_lower = store_name.toLowerCase().split(" ").join("")
+    console.log(store_name_lower)
+    var stores = {}
+    if (store_name_lower in ['amazon', 'costco', 'starmarket', 'targetgrocery', 'traderjoes', 'wholefoods']) {
+        const logo_image = '../resources/store-logos/' + store_name_lower
+        console.log(logo_image)
+    }
     return (
         <Modal
             animationType="slide"
@@ -18,26 +27,43 @@ const StoreView = (props) => {
                 this.setModalVisible(!modalVisible);
             }}>
             <View style={styles.modalView}>
-                <Text style={styles.title}>{store_name}</Text>
-                <View style={{flexDirection:'row', flex:1}}>
-                    <View style={{flexDirection:'column', flex:1, justifyContent:'center', alignItems:'center'}}>
-                        <View styles={styles.square}>
-                            <Text>Hello1</Text>
-                        </View>
-                        <View styles={styles.square}>
-                            <Text>Hello2</Text>
-                        </View>
-                    </View>
-                    <View style={{flexDirection:'column', flex:1, justifyContent:'center',alignItems:'center'}}>
-                        <View styles={styles.square}>
-                            <Text>Hello3</Text>
-                        </View>
-                        <View styles={styles.square}>
-                            <Text>Hello4</Text>
-                        </View>
+                <View style={{flexDirection:'row', justifyContent:'space-around', flex:1}}>
+                <Card.Title
+                title={store_name}
+                subtitle="0.5 mi away" //TODO: add accurate distance info 
+                left={(props) => <Avatar.Icon size={50} icon="food-apple" color='#813300' style={{backgroundColor:'transparent'}}/>}
+                style={{flex:1}}
+                />
+                </View>
+
+                <View style={{flexDirection:'row', flex:2, justifyContent:'center', alignItems:'center', backgroundColor: '#FFF6F0', borderRadius: 10, margin:10}}>
+                    <View styles={{}}>
+                        <IconButton
+                        icon="walk"
+                        size={20}/>
+                        <Text> 12 min</Text>
+
+                        <IconButton
+                        icon="train"
+                        size={20}/>
+                        <Text> 7 min</Text>
+
                     </View>
                 </View>
-                <View style={{flexDirection:'row', marginBottom: 30}}>
+                    
+                <View style={{flexDirection:'row', flex:2, backgroundColor: '#FFF6F0', alignItems:'center', margin: 10, justifyContent:'center', justifySelf:'center', borderRadius: 10}}>
+                    <View styles={{flexDirection:'column', flex:1, backgroundColor: '#FFF6F0', borderRadius: 10, margin: 5, justifyContent:'flex-start'}}>
+                        <Text style={{fontSize: 16}}> Items {"\n"} Available </Text>
+                        <Text style={styles.itemAvail}>{items_available}</Text>
+                        <Text style={styles.itemAvail}>━</Text>
+                        <Text style={styles.itemAvail}>18</Text>
+                    </View>
+                    <View styles={{flexDirection:'column', flex:1, backgroundColor: 'white', borderRadius: 10, margin: 5, justifyContent:'center', justifySelf:'center'}}>
+                        <Text>Total Cost:</Text>
+                        <Text style={{fontSize:32}}>${total_price}</Text>
+                    </View>
+                </View>
+                <View style={{flexDirection:'row', flex: .5, justifyContent:'center', justifySelf:'center', alignItems:'flex-end'}}>
                     <Button onPress={props.closeModal} title="Close"/>
                     <Button title="Start Trip"/>
                 </View>
@@ -48,12 +74,6 @@ const StoreView = (props) => {
   }
 
 const styles = StyleSheet.create({
-    square: {
-        height: 100,
-        width: 100,
-        backgroundColor: "#FFEBDD",
-        flex:1
-    },
     title: {
         // alignSelf:"flex-start",
         marginTop: 30,
@@ -61,27 +81,28 @@ const styles = StyleSheet.create({
         // "fontStyle": "normal",
         fontWeight: "700",
         fontSize: 25,
+        flex: 4,
+        alignSelf: 'center'
         // "color": "#000000"
     },
-    // container: {
-    //     flex: 1,
-    //     backgroundColor: '#fff',
-    //     alignItems: 'center',
-    //     justifyContent: 'center',
-    // },
     modalView: {
+        display: 'flex',
+        flexDirection: 'column',
         borderRadius: 10,
         // position: "absolute",
         alignSelf:"center",
-        justifyContent: "space-between",
+        justifyContent: "space-around",
+        padding:5,
         width: 330,
         height: 460,
         // justifyContent: "center",
-        alignItems: "center",
-        // "flex":1,
+        alignItems: "stretch",
         top: 234,
         backgroundColor: "#FFFFFF"
     },
+    itemAvail:{
+        fontSize: 28
+    }
 });
 
   export default StoreView;
