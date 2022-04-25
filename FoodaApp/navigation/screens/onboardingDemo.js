37f26eeb2e7862e06e81a783137155bfde0ae3bc
acastronuovo5 <55922639+acastronuovo5@users.noreply.github.com>
2022-04-25 10:50:37 -0400

    Fixed Fonts

--- FoodaApp/navigation/screens/onboardingDemo.js
+++ FoodaApp/navigation/screens/onboardingDemo.js
@@ -7,6 +7,9 @@ import Onboarding from 'react-native-onboarding-swiper';
 import { useNavigation } from '@react-navigation/native';
 import { TextInput } from 'react-native-paper';
 
+import AppLoading from 'expo-app-loading';
+import { useFonts, PTSerifCaption_400Regular} from '@expo-google-fonts/pt-serif-caption';
+
 import cherry from '../../assets/cherry.png'
 import leaf from '../../assets/leaf.png'
 import pear from '../../assets/pear.png'
@@ -16,107 +19,116 @@ const AddName = ({addName}) => {
   const [text, setText] = React.useState("");
   const navigation = useNavigation();
 
-  return (
-    <View>
-      <TextInput
-        label="Name"
-        value={text}
-        onChangeText={text => setText(text)}
-      />
-      <TouchableOpacity
-        //style={styles.addBtn}
-        //title={'I got it :)'}
-        //containerViewStyle={{ marginTop: 20 }}
-        //backgroundColor={'white'}
-        //borderRadius={5}
-        //textStyle={{ color: 'black' }}
-        onPress={() => {
-          addName(text);
-          setText('');
+  let [fontsLoaded] = useFonts({ PTSerifCaption_400Regular});
 
-        }} >
-          <Button
-            title={'I got it :)'}
-            titleStyles = {styles.title}
-            style = {styles.addBtn}
-            //containerViewStyle={{ marginTop: 20 }}
-            //backgroundColor={'white'}
-            //borderRadius={5}
-            textStyle={{
-              fontFamily: 'serif',
-              color: 'black',
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else { 
+  return (
+      <View>
+        <TextInput
+          label="Name"
+          value={text}
+          onChangeText={text => setText(text)}
+        />
+        <TouchableOpacity
+          //style={styles.addBtn}
+          //title={'I got it :)'}
+          //containerViewStyle={{ marginTop: 20 }}
+          //backgroundColor={'white'}
+          //borderRadius={5}
+          //textStyle={{ color: 'black' }}
+          onPress={() => {
+            addName(text);
+            setText('');
+
+          }} >
+            <Button
+              title={'I got it :)'}
+              titleStyles = {styles.title}
+              style = {styles.addBtn}
+              //containerViewStyle={{ marginTop: 20 }}
+              //backgroundColor={'white'}
+              //borderRadius={5}
+              textStyle={{
+                fontFamily: 'PTSerifCaption_400Regular',
+                color: 'black',
+                
+              }}
+              onPress={() => {
+                navigation.navigate("Profile", {name: addName})}
               
-            }}
-            onPress={() => {
-              navigation.navigate("Profile", {name: addName})}
-            
-            }
-          />
-        </TouchableOpacity>
-     </View>
-  );
-};
+              }
+            />
+          </TouchableOpacity>
+      </View>
+    )};
+  };
 
 
 const onboardingDemo = ({addName}) => {
   const navigation = useNavigation();
- 
-  return (
-    <Onboarding
-      onDone={() => console.log('done')}
-      showSkip = {false}
-      showNext = {false}
-      showDone = {false}
+  let [fontsLoaded] = useFonts({ PTSerifCaption_400Regular});
 
-      titleStyles={styles.title}
-      subTitleStyles={styles.subtitle}
-      bottomBarColor= '#FFEBDD'
-      bottomBarHeight={80}
-      imageContainerStyles= {{paddingBottom: 30, size:20}}
-      containerStyles={{paddingBottom: 250}}
-      pages={[
-        {
-          backgroundColor: '#FFEBDD',
-          image: <Image source={pear} size= {20}/>,
-          title: "Welcome to mart. Let's help you shop smarter! \n \n How it works: \n \n 1. Create your Grocery List",
-          subtitle: '',
-          color: '#813300',
-          
-        },
-        {
-          backgroundColor: '#FFEBDD',
-          image: <Image source= {cherry} size= {20} />,
-          title: '2. Select your grocery list from the drop down on the homepage',
-          subtitle: '',
-          paddingBottom: 0,
-        },
-        {
-          backgroundColor: '#FFEBDD',
-          image: <Image source={leaf} size= {20}  />,
-          title: '3. Choose your grocery store, check for item availability, trip time, and more!',
-          subtitle: "",
-        },
-        {
-          backgroundColor: '#FFEBDD',
-          image: <Image source={tomato} size= {20} />,
-          title: '4. Start your trip and find items with ease!',
-          subtitle: "",
-        },
-        {
-          backgroundColor: '#FFEBDD',
-          image: <Image source={tomato} size= {20}  />,
-          //titleStyles: {position: 'absolute'},
-          title: 'Enter your name here!',
-          subtitle: (
-            <View style = {styles.container}>
-              <AddName addName ={addName}/>
-            </View>
-              ),
-          
-        },
-      ]}
-    /> 
-    );
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else { 
+    return (
+      <Onboarding
+        onDone={() => console.log('done')}
+        showSkip = {false}
+        showNext = {false}
+        showDone = {false}
+
+        titleStyles={styles.title}
+        subTitleStyles={styles.subtitle}
+        bottomBarColor= '#FFEBDD'
+        bottomBarHeight={80}
+        imageContainerStyles= {{paddingBottom: 30, size:20}}
+        containerStyles={{paddingBottom: 250}}
+        pages={[
+          {
+            backgroundColor: '#FFEBDD',
+            image: <Image source={pear} size= {20}/>,
+            title: "Welcome to mart. Let's help you shop smarter! \n \n How it works: \n \n 1. Create your Grocery List",
+            subtitle: '',
+            color: '#813300',
+            
+          },
+          {
+            backgroundColor: '#FFEBDD',
+            image: <Image source= {cherry} size= {20} />,
+            title: '2. Select your grocery list from the drop down on the homepage',
+            subtitle: '',
+            paddingBottom: 0,
+          },
+          {
+            backgroundColor: '#FFEBDD',
+            image: <Image source={leaf} size= {20}  />,
+            title: '3. Choose your grocery store, check for item availability, trip time, and more!',
+            subtitle: "",
+          },
+          {
+            backgroundColor: '#FFEBDD',
+            image: <Image source={tomato} size= {20} />,
+            title: '4. Start your trip and find items with ease!',
+            subtitle: "",
+          },
+          {
+            backgroundColor: '#FFEBDD',
+            image: <Image source={tomato} size= {20}  />,
+            //titleStyles: {position: 'absolute'},
+            title: 'Enter your name here!',
+            subtitle: (
+              <View style = {styles.container}>
+                <AddName addName ={addName}/>
+              </View>
+                ),
+            
+          },
+        ]}
+      /> 
+      )};
   };
 
 const styles = StyleSheet.create({
@@ -124,7 +136,7 @@ const styles = StyleSheet.create({
     fontSize: 16,
     textAlign: "center",
     color: '#813300',
-    fontFamily: 'serif',
+    fontFamily: 'PTSerifCaption_400Regular',
     backgroundColor: '#FFEBDD',
     fontWeight: '400',
     //paddingBottom: 20,
