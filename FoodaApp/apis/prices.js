476db73393b085edac1f963f14a0c941b8cdc6a7
Daniel Delijani <danieldelijani@gmail.com>
2022-04-27 22:15:13 -0400

    target and trader joes working for one item

--- FoodaApp/apis/prices.js
+++ FoodaApp/apis/prices.js
@@ -36,27 +36,21 @@ const webScrapingApiClient = require('webscrapingapi');
 
 const client = new webScrapingApiClient("R0YDeuj4qe21H1q996Qgn03HBQ7utex8");
 // const xpath = require("xpath-html");
-// var fs = require('fs');
+
 const cheerio = require('cheerio');
 
-async function getWebPageTraderJoes(productName) {
+async function getTraderJoesPrice(productName) {
     let url = convertTraderJoesNameToUrl(productName);
-    // url = "https://www.traderjoes.com/home/search?q=milk&section=products&global=no";
 
     let response = await client.get(url, {
         'render_js': 1,
         'wait_until': 'domcontentloaded',
-        'wait_for_css': '.Section_section__oNcdC',
-        // 'wait_for': 6000
-        // 'device': 'desktop'
+        'wait_for_css': '.Section_section__oNcdC'
     }, {
-        // API Headers
-        // 'authorization': 'bearer test',
-        // Specify custom cookies to be passed to the request.
-        // 'cookie': 'test_cookie=abc; cookie_2=def'
     });
     if (response.success) {
         console.log("SUCCESS WOOHOO");
+        console.log(response);
         // console.log(response.response);
         // let html_file = Buffer.from(response.response.data, "utf-8");
         // fs.writeFileSync("test_html.html", html_file, {encoding: 'utf8',flag: 'w'});
@@ -67,19 +61,57 @@ async function getWebPageTraderJoes(productName) {
         let output = list_of_items.html();
         output = parseFloat(output.slice(1));
         console.log(output);
-        
-        // const node = xpath.fromPageSource(html).findElement('//*[@id="spa-root"]/div[1]/div[1]/div[2]/div[1]/div/div[2]/main/div/div/div/div[1]/ul/li/div/article[1]/div/div/div/span[1]');
-        // console.log(node);
-        // console.log(node.getTagName());
-        // console.log(node.getText());
-        return response.response;
+        return output;
     } else {
         console.log("API CALL ERROR");
         console.log(response.error);
     }
 }
 
-getWebPageTraderJoes("Turkey Bacon");
+
+
+async function getTargetPrice(productName) {
+    let url = convertTargetItemNameToUrl(productName);
+    console.log("URL:")
+    console.log(url);
+    let wait_for = '.ProductCardImageWrapper-sc-5fgvkn-0';// '.ProductCardImageWrapper-sc-5fgvkn-0';
+
+    let response = await client.get(url, {
+        'render_js': 1,
+        'wait_until': 'domcontentloaded',
+        // 'wait_for_css': wait_for,
+        'country': 'us',
+        'wait_for': 8000,
+        // 'proxy_type': 'residential',
+        'disable_stealth':1,
+        'timeout': 10000
+    }, {
+        // 'cookie': 'UserLocation=02446|42.350|-71.120|MA|US;GuestLocation=02215|42.340|-71.100|MA|US'
+    });
+    if (response.success) {
+        console.log("SUCCESS WOOHOO");
+        console.log(response);
+        
+        console.log(response.response);
+        var fs = require('fs');
+        let html_file = Buffer.from(response.response.data, "utf-8");
+        fs.writeFileSync("test_html.html", html_file, {encoding: 'utf8',flag: 'w'});
+        let html = response.response.data;
+        
+        const $ = cheerio.load(html);
+        const list_of_items = $($('.h-padding-r-tiny'));
+        let output = list_of_items.text();
+        output = parseFloat(output.split("$")[1])
+        console.log(output);
+        return output;
+    } else {
+        console.log("API CALL ERROR");
+        console.log(response.error);
+    }
+}
+
+// getTraderJoesPrice("Milk");
+getTargetPrice("Turkey Bacon");
 
 // OLD
 
@@ -98,24 +130,30 @@ function convertTargetItemNameToUrl(name) {
     return baseURL + nameString;
 }
 
-async function scrapeProductTarget(url) {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(url);
+// async function scrapeProductTarget(url) {
+//     const puppeteer = require('puppeteer');
+//     const browser = await puppeteer.launch({
+//         headless:false
+//     });
+//     const page = await browser.newPage();
+//     await page.goto(url);
+//     await page.waitForSelector('.ProductCardImageWrapper-sc-5fgvkn-0');
 
-    const [el] = await page.$x('//*[@id="pageBodyContainer"]/div[1]/div/div[4]/div/div[1]/div[2]/div/section/div/div[1]/div/div/div[2]/div/div/div[2]/div/div[1]/div/span');
-    const price = await el.getProperty('textContent')
-    const rawTxt = await price.jsonValue();
-    priceInt = parseFloat(rawTxt.slice(1));
-    console.log(priceInt);
-    browser.close()
-    return priceInt;
-}
+//     const [el] = await page.$x('//*[@id="pageBodyContainer"]/div[1]/div/div[4]/div/div[1]/div[2]/div/section/div/div[1]/div/div/div[2]/div/div/div[2]/div/div[1]/div/span');
+//     const price = await el.getProperty('textContent')
+//     const rawTxt = await price.jsonValue();
+//     priceInt = parseFloat(rawTxt.slice(1));
+//     console.log(priceInt);
+//     browser.close()
+//     return priceInt;
+// }
 
-async function getTargetPrice(name) {
-    url = convertTargetItemNameToUrl(name);
-    return scrapeProductTarget(url);
-}
+// async function getTargetPrice(name) {
+//     url = convertTargetItemNameToUrl(name);
+//     return scrapeProductTarget(url);
+// }
+
+// scrapeProductTarget("https://www.target.com/s?searchTerm=milk");
 
 // multiple items
 
@@ -123,32 +161,32 @@ function convertTargetItemNamesToUrls(names) {
     return names.map(convertTargetItemNameToUrl);
 }
 
-async function getTargetPrices(names) {
-    // URL: array of strings
-    // Returns: an array of prices
-    urls = convertTargetItemNamesToUrls(names);
-    prices = [];
+// async function getTargetPrices(names) {
+//     // URL: array of strings
+//     // Returns: an array of prices
+//     urls = convertTargetItemNamesToUrls(names);
+//     prices = [];
     
-    const browser = await puppeteer.launch({
-        headless:true
-    });
-    const page = await browser.newPage();
+//     const browser = await puppeteer.launch({
+//         headless:true
+//     });
+//     const page = await browser.newPage();
 
-    for (let i = 0; i < urls.length; i++) {
-        url = urls[i];
-        const promise = page.waitForNavigation({ waitUntil: 'networkidle2' });
-        await page.goto(url);
-        await promise;
-        const [el] = await page.$x('//*[@id="pageBodyContainer"]/div[1]/div/div[4]/div/div[1]/div[2]/div/section/div/div[1]/div/div/div[2]/div/div/div[2]/div/div[1]/div/span');
-        const price = await el.getProperty('textContent');
-        const rawTxt = await price.jsonValue();
-        priceInt = await parseFloat(rawTxt.slice(1));
-        await prices.push(priceInt);
-    }
-    browser.close()
-    console.log(prices)
-    return prices
-}
+//     for (let i = 0; i < urls.length; i++) {
+//         url = urls[i];
+//         const promise = page.waitForNavigation({ waitUntil: 'networkidle2' });
+//         await page.goto(url);
+//         await promise;
+//         const [el] = await page.$x('//*[@id="pageBodyContainer"]/div[1]/div/div[4]/div/div[1]/div[2]/div/section/div/div[1]/div/div/div[2]/div/div/div[2]/div/div[1]/div/span');
+//         const price = await el.getProperty('textContent');
+//         const rawTxt = await price.jsonValue();
+//         priceInt = await parseFloat(rawTxt.slice(1));
+//         await prices.push(priceInt);
+//     }
+//     browser.close()
+//     console.log(prices)
+//     return prices
+// }
 
 // TRADER JOES
 
@@ -168,33 +206,33 @@ function convertTraderJoesNamesToUrls(names) {
     return names.map(convertTraderJoesNameToUrl);
 }
 
-async function getTraderJoesPrices(names) {
-    // URL: array of strings
-    // Returns: an array of prices
-    urls = convertTraderJoesNamesToUrls(names);
-    prices = [];
+// async function getTraderJoesPrices(names) {
+//     // URL: array of strings
+//     // Returns: an array of prices
+//     urls = convertTraderJoesNamesToUrls(names);
+//     prices = [];
     
-    const browser = await puppeteer.launch({
-        headless:true
-    });
-    const page = await browser.newPage();
+//     const browser = await puppeteer.launch({
+//         headless:true
+//     });
+//     const page = await browser.newPage();
 
-    for (let i = 0; i < urls.length; i++) {
-        url = urls[i];
-        const promise = page.waitForNavigation({ waitUntil: 'domcontentloaded' });
-        await page.goto(url);
-        await promise;
-        await page.waitForSelector('.Section_section__oNcdC');
-        const [el] = await page.$x('//*[@id="spa-root"]/div[1]/div[1]/div[2]/div[1]/div/div[2]/main/div/div/div/div[1]/ul/li/div/article[1]/div/div/div/span[1]');
-        const price = await el.getProperty('textContent')
-        const rawTxt = await price.jsonValue();
-        priceInt = await parseFloat(rawTxt.slice(1));
-        await prices.push(priceInt);
-    }
-    browser.close();
-    console.log(prices);
-    return prices;
-}
+//     for (let i = 0; i < urls.length; i++) {
+//         url = urls[i];
+//         const promise = page.waitForNavigation({ waitUntil: 'domcontentloaded' });
+//         await page.goto(url);
+//         await promise;
+//         await page.waitForSelector('.Section_section__oNcdC');
+//         const [el] = await page.$x('//*[@id="spa-root"]/div[1]/div[1]/div[2]/div[1]/div/div[2]/main/div/div/div/div[1]/ul/li/div/article[1]/div/div/div/span[1]');
+//         const price = await el.getProperty('textContent')
+//         const rawTxt = await price.jsonValue();
+//         priceInt = await parseFloat(rawTxt.slice(1));
+//         await prices.push(priceInt);
+//     }
+//     browser.close();
+//     console.log(prices);
+//     return prices;
+// }
 
 // RANDOM, FOR CONSISTENCY
 
@@ -216,4 +254,4 @@ async function getUnimplementedPrices(names) {
 
 // export {getTargetPrices, getTraderJoesPrices, getUnimplementedPrices};
 
-export {getWebPageTraderJoes, getUnimplementedPrices};
+// export {getTraderJoesPrice, getUnimplementedPrices};
