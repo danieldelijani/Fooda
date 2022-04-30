fcac9666b808993b69bc3863c3b71b4e94ec3a2b
acastronuovo5 <55922639+acastronuovo5@users.noreply.github.com>
2022-04-30 15:19:17 -0400

    Added pictures and icons

--- FoodaApp/navigation/MainContainer.js
+++ FoodaApp/navigation/MainContainer.js
@@ -3,12 +3,16 @@ import { NavigationContainer, StackActions } from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 import GoogleMaps from './screens/GoogleMaps';
 import Profile from './screens/Profile';
 import GroceryListsNavigation from './GroceryListsNavigation';
 //import OnboardingDemo from './screens/OnboardingDemo';
 
+import mapIcon from '../assets/mapIcon.svg'
+import listIcon from '../assets/listIcon.svg'
+import profileIcon from '../assets/profileIcon.svg'
 
 
 const homeName = 'Home';
@@ -30,22 +34,27 @@ export default function MainContainer() {
         <NavigationContainer>
                 <Tab.Navigator
                     initialRouteName={
-                        onboarded ==true ? homeName : profileName}  //changed from homeName
+                        onboarded ==true ? GroceryListsNavigationName : profileName}  //changed from homeName
+                        
                 screenOptions={({route}) => ({
                     tabBarIcon: ({focused, color, size}) => {
                         let iconName;
                         let rn = route.name;
                         if (rn == homeName) { 
                             iconName = focused ? 'home' : 'home-outline';
+                            //iconName = mapIcon;
                         } else if (rn == GroceryListsNavigationName) {
                             iconName = focused ? 'list' : 'list-outline';
+                            //iconName = listIcon;
                         }
                         else if (rn == profileName) {
                             iconName = focused ? 'settings' : 'settings-outline';
+                            //iconName = profileIcon;
+                            
                         }
                         
 
-                        return <Ionicons name = {iconName} size = {size} color = {color}></Ionicons>
+                        return <Ionicons name = {iconName} size = {size} color = {color}></Ionicons> //Ionicons or Icon
                     },
                     "tabBarActiveTintColor": "sienna",
                     "tabBarInactiveTintColor": "sienna",
